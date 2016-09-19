535dab36c1be948540489bb6ea3196029848fc6d
maestrotype <adan0102@mail.ru>
2016-09-19 10:20:09 +0300

    changes 2.3

---
+++ dist/js/common.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+	// $(".drop-list").click( function() {
+
+	// 	$(this).toggleClass("open");
+
+	// });
+	// -- Scroll to id
+
+	$("a.scrolldown").mPageScroll2id();
+	// --EndScroll
+
+	// --addText
+	$("a#addText").click( function() {
+		var text = "<br /><br /> Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.";
+		$(".start p").append(text);
+		$(this).hide();
+
+		return false;
+	});
+
+	// --EndAddText
+
+	
+	$("#title-left").animated("bounceInLeft", "fadeOut");
+
+	$("#title-right").animated("bounceInRight", "fadeOut");
+
+	$("#left-block").animated("bounceInLeft", "fadeOut");
+
+	$("#right-block").animated("bounceInRight", "fadeOut");
+
+	// --click-menu
+
+	$(".toggle-mnu").click( function() {
+		$(this).toggleClass("cross");
+		$("nav").toggleClass("dark");
+		$("ul").toggleClass("open");
+		$(".scrolldown").toggle();
+		if($(window).screen()) {
+			$(".side-button").toggle();
+		}	
+	}
+		);
+
+	// --end-click
+
+});
+
