60a894e015efe811ab50abae834086591f427d86
maestrotype <adan0102@mail.ru>
2016-09-21 10:44:36 +0300

    changes 2.7

--- app/js/tabs.js
+++ app/js/tabs.js
@@ -1,8 +1,28 @@
   $( function() {
     $( "#tabs" ).tabs();
 
-    // $("#anhor-2").click( function() {
+    $("#anhor-1").click( function() {
 
-    // 	$(".project-left").css("transform" : "translateX(0)
-    // });
+    	$(".project-left").animated("lightSpeedIn", "fadeOut");
+    	$(".project-right").animated("zoomInRigh", "fadeOut");
+    	$(".project-center").animated("flipInY", "fadeOut");
+
+    });
+
+    $("#anhor-2").click( function() {
+
+    	$(".project-left").animated("lightSpeedIn", "fadeOut");
+    	$(".project-right").animated("zoomInRigh", "fadeOut");
+    	$(".project-center").animated("flipInY", "fadeOut");
+    	$(".project-up").animated("bounceInDown", "fadeOut");
+    	// $(".project-left").addClass("rotate");
+    });
+
+    $("#anhor-3").click( function() {
+
+    	$(".project-left").animated("lightSpeedIn", "fadeOut");
+    	$(".project-right").animated("zoomInRigh", "fadeOut");
+    	$(".project-center").animated("flipInY", "fadeOut");
+
+    });
   } );
